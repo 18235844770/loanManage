658a4ada203f97000d17fd6d94fa3e380d1079a4
huyuhang <1326825614@qq.com>
2025-10-24 18:22:15 +0800

    25/10/24

--- vue.config.js
+++ vue.config.js
@@ -84,14 +84,14 @@ const vueConfig = {
         pathRewrite: { '^/baidu-map': '' }
       },
       '/API': {
-        target: 'http://115.159.103.201/loans',
+        target: 'https://115.159.103.201/loans',
         changeOrigin: true,
         secure: false,
         pathRewrite: { '^/API': '' },
         // ✅ 添加伪造请求头配置
         onProxyReq (proxyReq, req, res) {
-          proxyReq.setHeader('Origin', 'http://115.159.103.201')
-          proxyReq.setHeader('Referer', 'http://115.159.103.201/')
+          proxyReq.setHeader('Origin', 'https://115.159.103.201')
+          proxyReq.setHeader('Referer', 'https://115.159.103.201/')
         },
         onError (err, req, res) {
           console.error('[Proxy Error]', err)
